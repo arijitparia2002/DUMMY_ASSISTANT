253a3437df32e8d47eb36a066c0ae6b07bb0a30b
Arijit Paria <arijitparia2020@gmail.com>
2021-11-28 15:50:05 +0530

    Remove unused modules

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # for more features -> https://www.geeksforgeeks.org/voice-assistant-using-python/
 
 from email import message
-from subprocess import TimeoutExpired
-from urllib import request
-from weakref import ProxyTypes
+# from subprocess import TimeoutExpired
+# from urllib import request
+# from weakref import ProxyTypes
 import pyttsx3 # text data into speech
 import datetime # date and time
 import speech_recognition as sr # speech from mic to text
@@ -36,10 +36,10 @@ def getvoice(voice):
     rate = engine.getProperty("rate")
     engine.setProperty("rate",190)
     if voice ==1: # male
-        engine.setProperty("voice",voices[0].id)
+        engine.setProperty("voice",voices[1].id)
         speak("hello , this is jarvis")
     if voice ==2: # female
-        engine.setProperty("voice", voices[1].id)
+        engine.setProperty("voice", voices[0].id)
         speak("hello , this is friday")
 
 #date time function
@@ -223,7 +223,7 @@ def run_jarvis():
                 try :
                     speak("searching...")
                     query = query.replace("wikipedia","") #replacing with empty string
-                    result = wikipedia.summary(query,sentences = 4) # read 4 sentences
+                    result = wikipedia.summary(query,sentences = 2) # read 4 sentences
                     print(result)
                     speak(result)
                 except Exception as e:
